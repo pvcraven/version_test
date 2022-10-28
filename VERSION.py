68b66371341d5852b94eb310842bb97717a7e71e
Paul Craven <paul@cravenfamily.com>
2022-10-27 19:58:28 -0500

    'Update'

--- VERSION.py
+++ VERSION.py
@@ -1,1 +1,13 @@
-VERSION = "2.7.1.dev1"
+import os
+
+
+# open text file in read mode
+text_file = open(f"{os.path.dirname(__file__)}\VERSION", "r")
+
+# read whole file to a string
+data = text_file.read().strip()
+
+# close file
+text_file.close()
+
+VERSION = data
